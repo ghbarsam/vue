ee69046e07f0b7728e46ad910eaa7a1ca28a0f0a
Ali Ghavidel <ghavidel.aliabadi@gmail.com>
2024-02-25 22:12:39 +0330

    two way binding usefull for inputs

--- app.js
+++ app.js
@@ -1,48 +1,14 @@
 Vue.createApp({
     data() {
         return {
-        percentage: 0,
-        start: true,
-        center: false,
-        end: false    
+        fullName: "",
+        email: "",
+        phone: "",
+        subject: "",
+        text: ""
         }
     },
     methods: {
-        increase(){
-            this.percentage === 100 ? null : this.percentage++;
-            if(this.percentage <= 30){
-                this.start = true;
-                this.center = false;
-                this.end = false;
-            }
-            if( this.percentage > 30 && this.percentage <= 70){
-                this.start = false;
-                this.center = true;
-                this.end = false;
-            }
-            if( this.percentage > 70){
-                this.start = false;
-                this.center = false;
-                this.end = true;
-            }
-        },
-        decrease(){
-            this.percentage === 0 ? null : this.percentage--;
-            if(this.percentage <= 30){
-                this.start = true;
-                this.center = false;
-                this.end = false;
-            }
-            if( this.percentage > 30 && this.percentage <= 70){
-                this.start = false;
-                this.center = true;
-                this.end = false;
-            }
-            if( 70 < this.percentage){
-                this.start = false;
-                this.center = false;
-                this.end = true;
-            }
-        }
+        
     },
 }).mount("#app")
