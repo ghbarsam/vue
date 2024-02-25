2f142389ce50d29878620801f43b5ed35b372efd
Ali Ghavidel <ghavidel.aliabadi@gmail.com>
2024-02-26 01:20:23 +0330

    use single file component (sfc)

---
+++ src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template>
+        <h1>ali</h1>
+        <br>
+        <p>ghavidel</p>
+</template>
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+<style scoped>
+h1{
+    color: red;
+}
+</style>
