559802af119a2b33d9047f93ae084e979b1f7593
Ali Ghavidel <ghavidel.aliabadi@gmail.com>
2024-02-25 23:46:22 +0330

    use computed for search

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 const app = Vue.createApp({
     data() {
         return {
-            display: "all"
+            display: "all",
+            contacts: [],
+            contact: {
+                name: "",
+                phone: "",
+                email: ""
+            },
+            search: ""
         }
     },
     methods: {
-        handleTabs(tab){
+        handleTabs(tab) {
             this.display = tab;
+        },
+        addContact() {
+            this.contacts.push(this.contact);
+            this.contact = {};
+            this.display = "all";
+            toastr.success('کاربر اضافه شد');
+        },
+        deleteContact(email) {
+            console.log(email);
+            this.contacts = this.contacts.filter((item) => item.email !== email);
         }
     },
+    computed: {
+        contactsWithFilter() {
+            if (this.search == "")
+                return [];
+
+            return this.contacts.filter((item) => item.email.includes(this.search))
+        }
+    }
 });
 
 app.mount("#app");
